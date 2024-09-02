2df34849373d67d562a00da55d5a8c3872d08819
woojoung <ywj981217@naver.com>
2024-09-02 14:58:47 +0900

    Refactor: vercel 배포시도 #1

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -8,7 +8,7 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
     <div className={`${poppinsFont.className}`}>
       <div className="notFound-wrapper">
         <div className="notFound-text">
-          <h1>오류 메세지:</h1>
+          <h1>오류 메세지 : </h1>
           <p className="text-p"> {error.message}</p>
           <p className="text-p-r">요청하신 내용을 처리하는 과정에서 오류가 발생했어요</p>
           <span className="text-p-l">We can't seem to find the request you're looking for.</span>
